f2d58cad7fb65b8c062bfa3a94e83e8210283f9f
syntrice <syntrice@protonmail.com>
2023-12-15 11:40:40 +0000

    update canon example

--- samples/isorhythm/canon-example.py
+++ samples/isorhythm/canon-example.py
@@ -1,17 +1,29 @@
+from copy import deepcopy
 import music21 as m21
 from composer_toolkit import tools, isorhythm, canon
 
 if __name__ == "__main__":
-    pitches = ["C4", "C4", "G4", "G4", "A4", "A4", "G4", "F4", "F4", "E4", "E4", "D4", "D4", "C4"]
-    durations = [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2]
+    pitches = ["C4","D4","E4","C4","B3","C4","A3","B3"]
+    durations = [8]
 
 
-    melody1 = isorhythm.create_isorhythm(pitches, durations).transpose(24)
-    melody2 = isorhythm.create_isorhythm(pitches, durations).transpose(12)
-    melody3 = isorhythm.create_isorhythm(pitches, durations).transpose(0)
-    melody4 = isorhythm.create_isorhythm(pitches, durations).transpose(-12)
+    melody1 = isorhythm.create_isorhythm(pitches, durations, color_offset=0).transpose(24)
+    melody2 = isorhythm.create_isorhythm(pitches, durations, color_offset=1).transpose(12)
+    melody3 = isorhythm.create_isorhythm(pitches, durations, color_offset=2).transpose(0)
+    melody4 = isorhythm.create_isorhythm(pitches, durations, color_offset=3).transpose(-12)
+
+    melody1.append(deepcopy(melody1))
+    melody2.append(deepcopy(melody2))
+    melody3.append(deepcopy(melody3))
+    melody4.append(deepcopy(melody4))
+    
+    melody1.insert(0, m21.instrument.Violin())
+    melody2.insert(0, m21.instrument.Violin())
+    melody3.insert(0, m21.instrument.Viola())
+    melody4.insert(0, m21.instrument.Violoncello())
     
     canon.canonize(melody1, melody2, melody3, melody4, delay_duration=2)
-
     score = tools.streams_to_score(melody1, melody2, melody3, melody4)
+
+    score.write("midi", fp="./output/canon-example.mid")
     score.show()
