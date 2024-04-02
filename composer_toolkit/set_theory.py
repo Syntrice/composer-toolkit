31315ab74e9ef692d23b0d6423368eaaa37eabac
syntrice <syntrice@protonmail.com>
2024-04-02 21:47:17 +0100

    Update set theory functions * modify set theory functions to work without streams. This is more useful for analysis purposes. In this way they are mostly wrapper functions for converting * modify set theory functions to only process one chord at a time. The chord can still be given as a music21 chord object or a list of pitches + add a function to calculate the interval of transposition between two pitch class sets. + add some documentation
    Other changes:
    - remove mutlidispatch as a dependency
    * lock file has been updated

--- composer_toolkit/set_theory.py
+++ composer_toolkit/set_theory.py
@@ -1,153 +1,182 @@
+""" 
+A set of functions that are useful when performing the analysis of atonal works using set theory.
+
+//TODO optimize and tidy code
+
+"""
+
 import itertools
-from matplotlib.pylab import normal
-from music21 import stream, chord, interval, pitch
+from music21 import chord
 from typing import List, Union, Sequence
-import numbers
 
-import music21
+def forte_class(
+    pcset: Union[chord.Chord, Sequence[Union[int, str]]]
+) -> str:
+    """Returns the Forte class of a pitch class set.
 
-from composer_toolkit.transformations import octave_shift
+    Args:
+        pcset: The pitch class set to analyze. Can be a Chord object or a sequence of pitch classes.
 
+    Returns:
+        The Forte class of the pitch class set.
+    """
+    if not isinstance(pcset, chord.Chord):
+        pcset = chord.Chord(pcset)
 
-def identify_prime_forms(
-    s: Sequence[Union[chord.Chord, Sequence[Union[int, str]]]],
-    transposed: bool = True,
-) -> stream.Stream:
+    return pcset.forteClass
 
-    st = stream.Stream()
+def prime_form(
+    pcset: Union[chord.Chord, Sequence[Union[int, str]]]
+) -> List[int]:
+    """Returns the prime form of a pitch class set.
 
-    for i in range(len(s)):
+    Args:
+        pcset: The pitch class set to analyze. Can be a Chord object or a sequence of pitch classes.
 
-        if not isinstance(s[i], chord.Chord):
-            c = chord.Chord(s[i])
-        else:
-            c = s[i]
+    Returns:
+        The prime form of the pitch class set.
+    """
+    if not isinstance(pcset, chord.Chord):
+        pcset = chord.Chord(pcset)
+
+    return pcset.primeForm
+    
+
+def normal_order(
+    pcset: Union[chord.Chord, Sequence[Union[int, str]]],
+    transposed: bool = False,
+) -> List[int]:
+    """Returns the normal order of a pitch class set.
+
+    Args:
+        pcset: The pitch class set to analyze. Can be a Chord object or a sequence of pitch classes.
+
+    Returns:
+        The normal order of the pitch class set.
+    """
+    if not isinstance(pcset, chord.Chord):
+        pcset = chord.Chord(pcset)
         
-        prime_form = c.primeForm
-        
-        if not transposed:
-            transposition = c.root().pitchClass
-            prime_form = [pitch.Pitch(midi = (pc + 60 + transposition)) for pc in prime_form]
+    if transposed:
+        transposition_interval = pcset.normalOrder[0]
+        return [(pc - transposition_interval) % 12 for pc in pcset.normalOrder]
+
+    return pcset.normalOrder
+
+# chromatically invert all intervals of a pitch class set around the root of the normal order
+def inverted(
+    pcset: Union[chord.Chord, Sequence[Union[int, str]]],
+    transposed: bool = False
+) -> List[int]:
+    """Inverts a pitch class set around the root of the normal order.
+
+    Args:
+        pcset: The pitch class set to invert. Can be a Chord object or a sequence of pitch classes.
+        transposed: If True, the pitch class set is transposed to T=0 (C)
+
+    Returns:
+        The inverted pitch class set.
+    """
+    if not isinstance(pcset, chord.Chord):
+        pcset = chord.Chord(pcset)
+    
+    # invert around the root of the normal order
+    normal_order = pcset.normalOrder
+    
+    root = normal_order[0]
+    inverted = [(root - pc) % 12 for pc in normal_order]
+    
+    inverted = chord.Chord(inverted)
             
-        c = chord.Chord(prime_form)
+    if transposed:
+        transposition_interval = inverted.normalOrder[0]
+        return [(pc - transposition_interval) % 12 for pc in inverted.normalOrder]
 
-        # add details
-        c.lyric = c.primeFormString
-        c.lyric += "\n" + c.forteClassTnI
-        c.lyric += "\n" + c.intervalVectorString
+    return inverted.normalOrder
 
-        # append to stream
-        st.append(c)
+def complement(
+    pcset: Union[chord.Chord, Sequence[Union[int, str]]],
+    transposed: bool = False
+) -> List[int]:
+    """Returns the complement of a pitch class set. That is, the set created from of all pitch classes not in the original set.
 
-    return st
+    Args:
+        pcset: The pitch class set to analyze. Can be a Chord object or a sequence of pitch classes.
+        transposed: If True, the pitch class set is transposed to T=0 (C)
+        
+    Returns:
+        The complement of the pitch class set.
+    """
+    
+    if not isinstance(pcset, chord.Chord):
+        pcset = chord.Chord(pcset)
+    
+    normal_order = pcset.normalOrder
+    
+    complement = []
+    for pc in set(range(12)):
+        if pc not in normal_order:
+            complement.append(pc)
+    
+    complement = chord.Chord(complement)
+    
+    if transposed:
+        transposition_interval = complement.normalOrder[0]
+        return [(pc - transposition_interval) % 12 for pc in complement.normalOrder]
+    
+    return complement.normalOrder
 
-def identify_normal_orders(
-    s: Sequence[Union[chord.Chord, Sequence[Union[int, str]]]],
-    tranposed: bool = False,
-) -> stream.Stream:
+def subsets(
+    pcset: Union[chord.Chord, Sequence[Union[int, str]]]
+) -> List[str]:
+    """Returns all subsets of a pitch class set.
 
-    st = stream.Stream()
-
-    for i in range(len(s)):
-
-        if not isinstance(s[i], chord.Chord):
-            c = chord.Chord(s[i])
-        else:
-            c = s[i]
+    Args:
+        pcset: The pitch class set to analyze. Can be a Chord object or a sequence of pitch.
+        transposed: If True, the pitch class set is transposed to T=0 (C).
+        
+    Returns:
+        A list of all subsets of the pitch class set, given in forte numbers.
+    """
+    
+    if not isinstance(pcset, chord.Chord):
+        pcset = chord.Chord(pcset)
+    
+    subsets = []
+    
+    found = set()
+    for cardinal in range(len(pcset.pitches)-1, 2, -1):
+        for combination in itertools.combinations(pcset.pitches, cardinal):
+            subset = chord.Chord(chord.Chord(combination))
             
-        normal_order = c.normalOrder
-        t = normal_order[0]
-        if tranposed:
-            normal_order = [(pc - t) % 12 for pc in normal_order]
-        else:
-            # convert pitch classes to strings, including octave register
-            highest = 0
-            _ = []
-            for pc in normal_order:
-                if pc < highest:
-                    pc += 12
-                
-                highest = pc
-                
-                _.append(pitch.Pitch(midi = pc + 60))
-                normal_order = _
-                    
-        c = chord.Chord(normal_order)        
-        
-        # add details
-        c.lyric = c.normalOrderString
-        c.lyric += "\n" + c.forteClass
-        c.lyric += "\n" + c.intervalVectorString
-        c.lyric += "\n" + "t = " + str(t)
+            if subset.primeFormString in found:
+                continue    
+            
+            found.add(subset.primeFormString)
+            
+            subsets.append(subset.forteClassTnI)
+            
+    return subsets
 
-        # append to stream
-        st.append(c)
+def t_operator(
+    pcset1: Union[chord.Chord, Sequence[Union[int, str]]],
+    pcset2: Union[chord.Chord, Sequence[Union[int, str]]],
+) -> int:
+    """Get the interval of transposition between two pitch class sets.
 
-    return st
+    Args:
+        pcset1 (Union[chord.Chord, Sequence[Union[int, str]]]): First pitch class set.
+        pcset2 (Union[chord.Chord, Sequence[Union[int, str]]]): Second pitch class set.
 
-def identify_subsets(
-    s: Sequence[Union[chord.Chord, Sequence[Union[int, str]]]],
-    tranposed: bool = False,
-) -> stream.Score:
-
-    st = stream.Stream()
+    Returns:
+        int: the interval of transposition between the two pitch class sets.
+    """
     
-    for i in range(len(s)):
-
-        if not isinstance(s[i], chord.Chord):
-            c = chord.Chord(s[i])
-        else:
-            c = s[i]
+    if not isinstance(pcset1, chord.Chord):
+        pcset1 = chord.Chord(pcset1)
         
-        found = set()
-        for cardinal in range(len(c.pitches)+1, 2, -1):
-            for combination in itertools.combinations(c.pitches, cardinal):
-                subset = chord.Chord(chord.Chord(combination))
-                
-                subset.lyric = subset.normalOrderString
-                subset.lyric += "\n" + subset.forteClassTnI
-                subset.lyric += "\n" + subset.intervalVectorString
-                
-                if subset.primeFormString not in found:
-                    found.add(subset.primeFormString)
-                    st.append(subset)
-                
-        # remove duplicates
+    if not isinstance(pcset2, chord.Chord):
+        pcset2 = chord.Chord(pcset2)
         
-        
-    return st
-
-def identify_complement(
-    s: Sequence[Union[chord.Chord, Sequence[Union[int, str]]]],
-    tranposed: bool = False,
-) -> stream.Score:
-
-    st = stream.Stream()
+    return pcset2.normalOrder[0] - pcset1.normalOrder[0]
     
-    for i in range(len(s)):
-
-        if not isinstance(s[i], chord.Chord):
-            c = chord.Chord(s[i])
-        else:
-            c = s[i]
-        
-        
-        if tranposed:
-            prime_form = c.primeForm
-        
-        
-        prime_form = c.normalOrder 
-        complement = []
-    
-        for pc in set(range(12)):
-            if pc not in prime_form:
-                complement.append(pc)
-        
-        complement = chord.Chord(complement)
-        complement.lyric = complement.normalOrderString
-        complement.lyric += "\n" + complement.forteClass
-        complement.lyric += "\n" + complement.intervalVectorString
-        
-        st.append(complement)
-                    
-    return st
